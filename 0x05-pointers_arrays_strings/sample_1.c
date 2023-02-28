f7e1b37587f0cc6c2ad614aeffaa01cd36374f01
Snorine <thatgalsnow@gmail.com>
2023-02-28 06:19:02 -0800

    Sample

---
+++ 0x05-pointers_arrays_strings/sample_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main (void)
+{       
+	int i;
+	 
+	printf("the size of int on my machine is %lu\n", sizeof(int));
+	printf("the size of char on my machine is %lu\n", sizeof(char));
+	printf("the size of float on my machine is %lu\n", sizeof(float));
+	printf("the size of i on my machine is %lu\n", sizeof(i));
+	return (0);
+}
