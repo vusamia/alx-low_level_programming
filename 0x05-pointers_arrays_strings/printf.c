bb3685bf329bf6cd5fab2616eebc49d2fb2e6045
Snorine <thatgalsnow@gmail.com>
2023-02-28 06:48:07 -0800

    Sample 2

---
+++ 0x05-pointers_arrays_strings/printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int number;
+
+	printf("Enter a number:");
+	scanf("%d",&number);
+	printf("Cube of number is: %d\n", number*number*number);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/sample_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	char j;
+
+	printf("Address of variable i: %p\n", &i);
+	printf("Address of variable 'j': %p\n", &j);
+	return (0);
+}
