e2fe19312e8d66ffa518e023d861a58119fb6b23
Snorine <thatgalsnow@gmail.com>
2023-03-01 16:36:41 -0800

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  *main - a C program that prints a line to the standard error
+  *Return: 1 (success)
+  */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	return (1);
+}
