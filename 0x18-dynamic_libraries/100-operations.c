6b12dc3feb760960a0866c24eca80152ccc59c96
Snorine Vusamia <thatgalsnow@gmail.com>
2023-06-22 18:35:39 -0700

    advanced

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: 2nd number
+ * Return: (a + b)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - diff btw two numbers
+ * @a: first number
+ * @b: 2nd number
+ * Return: (a - b)
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: 2nd number
+ * Return: (a * b)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divides two numbers
+ * @a: first number
+ * @b: 2nd number
+ * Return: (a / b)
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - modulo of two numbers
+ * @a: first number
+ * @b: 2nd number
+ * Return: (a % b)
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
